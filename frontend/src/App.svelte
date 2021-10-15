e69c74de6f8080d785272922d5ef6da2289c08d1
CStumph <cjstumph@gmail.com>
2021-10-15 00:17:14 -0700

    add some reactivity

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,10 +1,11 @@
 <script>
+	import MousePos from './components/mouse-position.svelte';
 	export let name;
 </script>
 
 <main>
 	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<MousePos/>
 </main>
 
 <style>
@@ -12,6 +13,7 @@
 		text-align: center;
 		padding: 1em;
 		max-width: 240px;
+		height: 100%;
 		margin: 0 auto;
 	}
 
@@ -25,6 +27,7 @@
 	@media (min-width: 640px) {
 		main {
 			max-width: none;
+			width: 100%;
 		}
 	}
 </style>
